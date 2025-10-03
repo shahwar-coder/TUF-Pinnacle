fe3c11d56659565dceec33edfd8f15b8b95ebd42
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-10-03 21:12:22 +0530

    Update pattern_1.py

--- pattern_easy/pattern_1.py
+++ pattern_easy/pattern_1.py
@@ -1,1 +1,21 @@
+'''
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
 
+*****
+*****
+*****
+*****
+*****
+'''
+
+class Solution:
+    def pattern1(self, n):
+        for _ in range(1,n+1):
+            print('*'*n)
+
+# _ instead of i for looping because i will not get used if introduced.
+# you might also return the pattern string if not asked to print eg. 
+
+'''
+return '\n'.join('*' * n for _ in range(1, n+1))
+'''
