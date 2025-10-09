95bd4509d0677da1195de35a33415bd2c538b636
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-10-09 17:12:18 +0530

    Create 01_take_inp_print.py

---
+++ language_basics/01_take_inp_print.py
@@ -0,0 +1,7 @@
+'''
+Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+'''
+
+class Solution:
+    def printNumber(self):
+        print(input())
