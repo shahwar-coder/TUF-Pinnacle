cdfb733589fe28a0e891ec449fe04eae04a8236c
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-10-09 17:28:24 +0530

    Create 05_sum_1st_last_array.py

---
+++ language_basics/05_sum_1st_last_array.py
@@ -0,0 +1,7 @@
+'''
+Given an integer array nums, return the sum of the 1st and last element of the array.
+'''
+
+class Solution:
+    def sumOfFirstAndLast(self, nums):
+        return nums[0]+nums[-1]
